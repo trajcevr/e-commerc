c096f9fdb7668a0625e04bfb334a07082baad31e
Robert <trajcevr@yahoo.com>
2025-02-11 20:27:52 +0100

    angular basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HeaderComponent } from "./layout/header/header.component";
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [HeaderComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  
+  baseUrl = 'https://localhost:5001/api/'; 
+  private http = inject(HttpClient);
+  title = 'Skinet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log("complete")
+    });
+  }
+
 }
