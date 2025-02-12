f347d53b3bbf43d1d3f407bb836d365b0f1a48bc
Robert <trajcevr@yahoo.com>
2025-02-12 17:25:44 +0100

    user interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [HeaderComponent], 
+  imports: [HeaderComponent, ShopComponent, MatPaginatorModule ], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  
-  baseUrl = 'https://localhost:5001/api/'; 
-  private http = inject(HttpClient);
+
+export class AppComponent {
   title = 'Skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log("complete")
-    });
-  }
-
 }
