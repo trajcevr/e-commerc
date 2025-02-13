f78869d80c2e23bdfb8639cf01331a6d1ff6a442
Robert <trajcevr@yahoo.com>
2025-02-13 12:53:14 +0100

    error handaling and loading

---
+++ client/src/app/features/test-error/test-error.component.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-test-error',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent {
+  baseUrl = "https://localhost:5001/api/";
+  private http = inject(HttpClient);
+  validationErrors?: string[];
+
+  get404Error() {
+    this.http.get(this.baseUrl + "buggy/notfound").subscribe({
+      next: response => console.log(response),
+      error: error => console.error('404 Error:', error)
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + "buggy/badrequest").subscribe({
+      next: response => console.log(response),
+      error: error => console.error('400 Error:', error)
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + "buggy/unauthorized").subscribe({
+      next: response => console.log(response),
+      error: error => console.error('401 Error:', error)
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + "buggy/internalerror").subscribe({
+      next: response => console.log(response),
+      error: error => console.error('500 Error:', error)
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + "buggy/validationerror", {}).subscribe({
+      next: response => console.log(response),
+      error: error => this.validationErrors = error
+    });
+  }
+}
