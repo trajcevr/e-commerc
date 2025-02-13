37dc32a2fb06edef023226be65402a78e4f3200a
Robert <trajcevr@yahoo.com>
2025-02-13 14:32:49 +0100

    redis/shoppingCart

---
+++ Infrastructure/Services/CartService.cs
@@ -0,0 +1,32 @@
+using System.Text.Json;
+using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+
+namespace Infrastructure.Services;
+
+public class CartService(IConnectionMultiplexer redis) : ICartService
+{
+    private readonly IDatabase _database = redis.GetDatabase();
+
+    public async Task<bool> DeleteCartAsync(string key)
+    {
+        return await _database.KeyDeleteAsync(key);
+    }
+
+    public async Task<ShoppingCart?> GetCartAsync(string key)
+    {
+        var data = await _database.StringGetAsync(key);
+        return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<ShoppingCart>(data!);
+    }
+
+    public async Task<ShoppingCart?> SetCartAsync(ShoppingCart cart)
+    {
+        var serializedCart = JsonSerializer.Serialize(cart);
+        var isSaved = await _database.StringSetAsync(cart.Id, serializedCart, TimeSpan.FromDays(30)); 
+
+        if (!isSaved) return null;
+
+        return await GetCartAsync(cart.Id);
+    }
+}
